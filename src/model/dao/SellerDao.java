0a2c71a3fcf1ef5850a2caae37d570b4c53f2a74
fhellipevalentin <fhellipereis@outlook.com>
2021-03-01 18:33:36 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,13 @@
 package model.dao;
 
-public interface SellerDao {
+import java.util.List;
 
+import model.entities.Seller;
+
+public interface SellerDao {
+	void insert (Seller obj);
+	void update (Seller obj);
+	void deleteByID(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
 }
