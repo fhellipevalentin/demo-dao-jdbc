734ef51f790cd9e11915d4261b7dd8cb197ab915
fhellipevalentin <fhellipereis@outlook.com>
2021-03-04 12:40:04 -0300

    insert department implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -16,13 +16,18 @@ public class Program2 {
 		System.out.println(department);
 		
 		System.out.println();
-		
 		System.out.println("=== TEST 2: Department findAll ===");
 		List<Department> list = departmentDao.findAll();
 		
 		for (Department item : list) {
 			System.out.println(item);
 		}
+		
+		System.out.println();
+		System.out.println("=== TEST 3: Department insert ===");
+		Department newdepartment = new Department(null, "Food");
+		departmentDao.insert(newdepartment);
+		System.out.println("Inserted! New ID: " + newdepartment.getId());
 		
 	}
 
